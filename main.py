1a35e143180dd3e2680296cdf6ac1a927b460c3f
Sunny223dev <165983200+Sunny223dev@users.noreply.github.com>
2024-05-07 17:24:20 -0400

    Add Background, SideBar, Profile-Icon, and Logo all to 'main.py'.
    Added two Images to Assets, and made folders in Assets.

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
+# Needed imports for code to work
 import tkinter as tk
 
 # Create the window
 window = tk.Tk()
 
 # Set the title of the window
-window.title("Cryption")
+window.title("Cryption - Password Manager")
 
 # Size of the window
-window.geometry("1000x500")
+window.geometry("1200x650")
 
-icon_path = r'C:\Users\sunny\OneDrive\Desktop\Cryption\Assets\cryption---logo.ico'
+# Make window not resizable
+window.resizable(False, False)
+
+# Set icon of window
+icon_path = r'Assets\Other - Assets\cryption-logo.ico'
 icon = tk.PhotoImage(file=icon_path)
-
-# Set the window icon
 window.iconphoto(True, icon) 
 
 # Sidebar
-sidebar_frame = tk.Frame(window, bg="#37474f", width=100)
+sidebar_frame = tk.Frame(window, bg="#37474f", width=120)
 sidebar_frame.pack(side="left", fill="y")
+sidebar_frame.pack_propagate(0)
 
-# Bakcground color
+# Adds profile account icon to the sidebar
+profile_account_icon_path = r'Assets\Sidebar - Icons\profile-account-icon.ico'
+profile_account_icon = tk.PhotoImage(file=profile_account_icon_path)
+profile_account_icon = profile_account_icon.subsample(7)
+profile_account_icon_label = tk.Label(sidebar_frame, image=profile_account_icon, bg="#37474f")
+profile_account_icon_label.pack(pady=10)
+
+# Background color
 window.configure(background='#263238')
 
 # Content that will be shown in the window
 label = tk.Label(window, text="hm no")
 label.pack()
 
+# It do be loopin
 window.mainloop()
