b472a746b5bab618d16449685e26c98196c84fd5
Sunny223dev <165983200+Sunny223dev@users.noreply.github.com>
2024-05-08 14:42:24 -0400

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # Needed imports for code to work
 import tkinter as tk
 
+# Centers Window
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
 # Create the window
 window = tk.Tk()
 
@@ -9,6 +20,9 @@ window.title("Cryption - Password Manager")
 
 # Size of the window
 window.geometry("1200x650")
+
+# Centers window
+center_window(window)
 
 # Make window not resizable
 window.resizable(False, False)
@@ -29,6 +43,13 @@ profile_account_icon = tk.PhotoImage(file=profile_account_icon_path)
 profile_account_icon = profile_account_icon.subsample(7)
 profile_account_icon_label = tk.Label(sidebar_frame, image=profile_account_icon, bg="#37474f")
 profile_account_icon_label.pack(pady=10)
+
+# Adds lock add icon to the sidebar
+lock_add_icon_path = r'Assets\Sidebar - Icons\lock-add-icon.ico'
+lock_add_icon = tk.PhotoImage(file=lock_add_icon_path)
+lock_add_icon = lock_add_icon.subsample(7)
+lock_add_icon_label = tk.Label(sidebar_frame, image=lock_add_icon, bg="#37474f")
+lock_add_icon_label.pack(pady=10)
 
 # Background color
 window.configure(background='#263238')
